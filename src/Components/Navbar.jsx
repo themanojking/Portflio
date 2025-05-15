831cf36c7c0ce9ecc9e8b53bf99346fdcec05298
themanojking <kmanoj71737@gmail.com>
2025-05-15 19:08:06 +0530

    refactor: restructure Navbar component for improved layout and responsiveness

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,69 +9,79 @@ function Navbar() {
 
   return (
     <>
-      <header className="container mx-auto md:mx-8 lg:mx-0 flex justify-between items-center p-5 w-full bg-white shadow-md shadow-violet-800 rounded-b-2xl fixed top-0 z-50">
-        <div className="flex items-center gap-2">
-          <div className="text-2xl font-bold flex items-center">
-            <IoIosPerson />
+      <div className="fixed w-full"> 
+        <header className="flex justify-between items-center p-5 w-full bg-white shadow-md shadow-violet-800 rounded-b-2xl ">
+          <div className="flex items-center gap-2">
+            <div className="text-2xl font-bold flex items-center">
+              <IoIosPerson />
+            </div>
+            <div>
+              <h1 className="text-2xl font-bold">Manoj Kumar</h1>
+            </div>
           </div>
-          <div>
-            <h1 className="text-2xl font-bold">Manoj Kumar</h1>
-          </div>
-        </div>
 
-        <nav className="lg:flex hidden items-center gap-16 scroll-smooth">
-          <a href="#Home">
-            <h2 className="text-xl font-bold hover:text-violet-800">Home</h2>
-          </a>
-          <a href="#About">
-            <h2 className="text-xl font-bold hover:text-violet-800">About</h2>
-          </a>
-          <a href="#Skills">
-            <h2 className="text-xl font-bold hover:text-violet-800">Skills</h2>
-          </a>
-          <a href="#Myproject">
-            <h2 className="text-xl font-bold hover:text-violet-800">
-              Projects
-            </h2>
-          </a>
-          <a href="#Letstalk">
-            <h2 className="text-xl font-bold hover:text-violet-800">Contact</h2>
-          </a>
-        </nav>
-
-        <button
-          className="lg:hidden text-2xl flex items-center"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? <FaTimes /> : <FaBars />}
-        </button>
-
-        {isOpen && (
-          <div className="absolute top-16 left-0 w-full lg:hidden bg-white flex flex-col items-center gap-5 py-5 ">
-            <a href="#Home" onClick={() => setIsOpen(false)}>
+          <nav className="lg:flex hidden items-center gap-16 scroll-smooth">
+            <a href="#Home">
               <h2 className="text-xl font-bold hover:text-violet-800">Home</h2>
             </a>
-            <a href="#About" onClick={() => setIsOpen(false)}>
+            <a href="#About">
               <h2 className="text-xl font-bold hover:text-violet-800">About</h2>
             </a>
-            <a href="#Skills" onClick={() => setIsOpen(false)}>
+            <a href="#Skills">
               <h2 className="text-xl font-bold hover:text-violet-800">
                 Skills
               </h2>
             </a>
-            <a href="#Myproject" onClick={() => setIsOpen(false)}>
+            <a href="#Myproject">
               <h2 className="text-xl font-bold hover:text-violet-800">
-                Project
+                Projects
               </h2>
             </a>
-            <a href="#Letstalk" onClick={() => setIsOpen(false)}>
+            <a href="#Letstalk">
               <h2 className="text-xl font-bold hover:text-violet-800">
                 Contact
               </h2>
             </a>
-          </div>
-        )}
-      </header>
+          </nav>
+
+          <button
+            className="lg:hidden text-2xl flex items-center"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? <FaTimes /> : <FaBars />}
+          </button>
+
+          {isOpen && (
+            <div className="absolute top-16 left-0 w-full lg:hidden bg-white flex flex-col items-center gap-5 py-5 ">
+              <a href="#Home" onClick={() => setIsOpen(false)}>
+                <h2 className="text-xl font-bold hover:text-violet-800">
+                  Home
+                </h2>
+              </a>
+              <a href="#About" onClick={() => setIsOpen(false)}>
+                <h2 className="text-xl font-bold hover:text-violet-800">
+                  About
+                </h2>
+              </a>
+              <a href="#Skills" onClick={() => setIsOpen(false)}>
+                <h2 className="text-xl font-bold hover:text-violet-800">
+                  Skills
+                </h2>
+              </a>
+              <a href="#Myproject" onClick={() => setIsOpen(false)}>
+                <h2 className="text-xl font-bold hover:text-violet-800">
+                  Project
+                </h2>
+              </a>
+              <a href="#Letstalk" onClick={() => setIsOpen(false)}>
+                <h2 className="text-xl font-bold hover:text-violet-800">
+                  Contact
+                </h2>
+              </a>
+            </div>
+          )}
+        </header>
+      </div>
     </>
   );
 }
